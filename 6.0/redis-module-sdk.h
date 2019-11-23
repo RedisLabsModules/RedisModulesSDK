4e9d779ab4eccb8a74cf15a2f14e4c917209d88c
rafie <rafi@redislabs.com>
2019-11-23 19:16:57 +0200

    Introduced 5.0 & 6.0 SDK versions

---
+++ 6.0/redis-module-sdk.h
@@ -0,0 +1,9 @@
+
+#pragma once
+
+#define REDISMODULE_XAPI_ENTERPRISE(X) \
+    X(int, AvoidReplicaTraffic, ())
+
+#define REDISMODULE_XAPI_EXTENSIONS(X) REDISMODULE_XAPI_ENTERPRISE(X)
+
+#include "RedisModuleSDK/6.0/redismodule.h"
