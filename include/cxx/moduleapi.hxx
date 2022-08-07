18c9585385165408ef35b90ae69a1b4ce9af37a8
Ephraim Feldblum <ephraim.feldblum@redis.com>
2022-08-07 09:27:09 +0300

    morning 7/8/22

--- include/cxx/moduleapi.hxx
+++ include/cxx/moduleapi.hxx
@@ -7,51 +7,36 @@ namespace RedisModule {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-struct Module {
-	void *Alloc(size_t bytes);
-	void *Calloc(size_t nmemb, size_t size);
-	void *Realloc(void *ptr, size_t bytes);
-	void Free(void *ptr);
+namespace Alloc {
+	void *Alloc(size_t bytes) {
+		return RedisModule_Alloc(bytes);
+	}
+	void *Calloc(size_t nmemb, size_t size) {
+		return RedisModule_Calloc(nmemb, size);
+	}
+	void *Realloc(void *ptr, size_t bytes) {
+		return RedisModule_Realloc(ptr, bytes);
+	}
+	void Free(void *ptr) {
+		RedisModule_Free(ptr);
+	}
+	char *Strdup(const char *str) {
+		return RedisModule_Strdup(str);
+	}
+	void *PoolAlloc(Context &ctx, size_t bytes) {
+		return RedisModule_PoolAlloc(ctx, bytes);
+	}
+} // namespace Alloc
 
-	char *Strdup(const char *str);
+struct Module {
+
 	int GetApi(const char *, void *);
 	long long Milliseconds(void);
-
-	const char *StringPtrLen(const RedisModuleString *str, size_t *len);
-	int StringToLongLong(const RedisModuleString *str, long long *ll);
-	int StringToDouble(const RedisModuleString *str, double *d);
-	int StringCompare(RedisModuleString *a, RedisModuleString *b);
 
 	void CloseKey(RedisModuleKey *kp);
 	int KeyType(RedisModuleKey *kp);
 	size_t ValueLength(RedisModuleKey *kp);
-	int ListPush(RedisModuleKey *kp, int where, RedisModuleString *ele);
-	RedisModuleString *ListPop(RedisModuleKey *key, int where);
 	int DeleteKey(RedisModuleKey *key);
-	int StringSet(RedisModuleKey *key, RedisModuleString *str);
-	char *StringDMA(RedisModuleKey *key, size_t *len, int mode);
-	int StringTruncate(RedisModuleKey *key, size_t newlen);
-	mstime_t GetExpire(RedisModuleKey *key);
-	int SetExpire(RedisModuleKey *key, mstime_t expire);
-	int HashSet(RedisModuleKey *key, int flags, ...);
-	int HashGet(RedisModuleKey *key, int flags, ...);
-	int ModuleTypeSetValue(RedisModuleKey *key, RedisModuleType *mt, void *value);
-	RedisModuleType *ModuleTypeGetType(RedisModuleKey *key);
-	void *ModuleTypeGetValue(RedisModuleKey *key);
-
-	int ZsetAdd(RedisModuleKey *key, double score, RedisModuleString *ele, int *flagsptr);
-	int ZsetIncrby(RedisModuleKey *key, double score, RedisModuleString *ele, int *flagsptr, double *newscore);
-	int ZsetScore(RedisModuleKey *key, RedisModuleString *ele, double *score);
-	int ZsetRem(RedisModuleKey *key, RedisModuleString *ele, int *deleted);
-	void ZsetRangeStop(RedisModuleKey *key);
-	int ZsetFirstInScoreRange(RedisModuleKey *key, double min, double max, int minex, int maxex);
-	int ZsetLastInScoreRange(RedisModuleKey *key, double min, double max, int minex, int maxex);
-	int ZsetFirstInLexRange(RedisModuleKey *key, RedisModuleString *min, RedisModuleString *max);
-	int ZsetLastInLexRange(RedisModuleKey *key, RedisModuleString *min, RedisModuleString *max);
-	RedisModuleString *ZsetRangeCurrentElement(RedisModuleKey *key, double *score);
-	int ZsetRangeNext(RedisModuleKey *key);
-	int ZsetRangePrev(RedisModuleKey *key);
-	int ZsetRangeEndReached(RedisModuleKey *key);
 
 	RedisModuleCtx *GetContextFromIO(RedisModuleIO *io);
 	void SaveUnsigned(RedisModuleIO *io, uint64_t value);
@@ -110,7 +95,6 @@ struct Module {
 	int IsKeysPositionRequest(RedisModuleCtx *ctx);
 	void KeyAtPos(RedisModuleCtx *ctx, int pos);
 	unsigned long long GetClientId(RedisModuleCtx *ctx);
-	void *PoolAlloc(RedisModuleCtx *ctx, size_t bytes);
 	RedisModuleType *CreateDataType(RedisModuleCtx *ctx, const char *name, int encver, RedisModuleTypeMethods *typemethods);
 	void Log(RedisModuleCtx *ctx, const char *level, const char *fmt, ...);
 	int StringAppendBuffer(RedisModuleCtx *ctx, RedisModuleString *str, const char *buf, size_t len);
@@ -187,31 +171,23 @@ struct SafeContext {
 
 //---------------------------------------------------------------------------------------------
 
-enum KEYTYPE {
-	EMPTY,
-	STRING,
-	LIST,
-	HASH,
-	SET,
-	ZSET,
-	MODULE,
-	STREAM
-};
 class RMType {
 public:
-	RMType(KEYTYPE _type = EMPTY) : m_type{_type} {}
+	RMType(Context &ctx, const char *name, int encver, RedisModuleTypeMethods *typemethods)
+		: _type(RedisModule_CreateDataType(ctx, name, encver, typemethods))
+	{ }
+	RMType(RedisModuleType *type) : _type(type) {}
 
-	KEYTYPE GetType() const { return m_type; }
-	void SetType(KEYTYPE _type) { m_type = _type; }
-	operator KEYTYPE() const { return m_type; }
+	operator RedisModuleType *() { return _type; }
+	operator const RedisModuleType *() const { return _type; }
 
 private:
-	enum KEYTYPE m_type;
+	RedisModuleType *_type;
 };
 
 //---------------------------------------------------------------------------------------------
 
-class String : RMType {
+class String {
 public:
     String(Context &ctx, const char *ptr, size_t len)
         : _str(RedisModule_CreateString(ctx, ptr, len))
@@ -222,7 +198,9 @@ public:
         : _str(rm_str)
     { }
 
-    ~String() { RedisModule_FreeString(/* _ctx ?? */, _str); }
+    ~String() {
+		RedisModule_FreeString(/* _ctx ?? */, _str);
+	}
 	
 	const char *PtrLen(size_t &len) const {
         return RedisModule_StringPtrLen(_str, &len);
@@ -258,21 +236,40 @@ public:
 	Key(Context &ctx, String &keyname, int mode) // OpenKey
 		: _key((RedisModuleKey *)RedisModule_OpenKey(ctx, keyname, mode))
 	{ }
-	~Key(); // CloseKey
-	int DeleteKey();
-	
-	int Type();
-	
-	size_t ValueLength();
-	
-	mstime_t GetExpire();
-	int SetExpire(mstime_t expire);
-	
-	RMType& GetType();
-	const RMType& GetType() const;
+	Key(RedisModuleKey *key) : _key(key) { }
 
-	void *GetValue() { return RedisModule_ModuleTypeGetValue(_key); }
-	int SetValue(RedisModuleType *mt, void *value) { return RedisModule_ModuleTypeSetValue(_key, mt, value); }
+	~Key() { // CloseKey
+		RedisModule_CloseKey(_key);
+	}
+	int DeleteKey() {
+		return RedisModule_DeleteKey(_key);
+	}
+	
+	int Type() {
+		return RedisModule_KeyType(_key);
+	}
+	
+	size_t ValueLength() {
+		return RedisModule_ValueLength(_key);
+	}
+	
+	mstime_t GetExpire() {
+		return RedisModule_GetExpire(_key);
+	}
+	int SetExpire(mstime_t expire){
+		return RedisModule_SetExpire(_key, expire);
+	}	
+
+	RMType GetType() {
+		return RedisModule_ModuleTypeGetType(_key);
+	}
+
+	void *GetValue() {
+		return RedisModule_ModuleTypeGetValue(_key);
+	}
+	int SetValue(RMType &mt, void *value) {
+		return RedisModule_ModuleTypeSetValue(_key, mt, value);
+	}
 
 	operator RedisModuleKey *() { return _key; }
 	operator const RedisModuleKey *() const { return _key; }
@@ -282,6 +279,8 @@ protected:
 };
 
 class StringKey : Key {
+	StringKey(Context &ctx, String &keyname, int mode) : Key(ctx, keyname, mode) {}
+
 	int Set(String &other) {
         return RedisModule_StringSet(_key, other);
     }
@@ -291,36 +290,66 @@ class StringKey : Key {
 	int Truncate(size_t newlen) { 
         return RedisModule_StringTruncate(_key, newlen);
     }
-}
-
-//---------------------------------------------------------------------------------------------
-
-struct Zset : Type {
-	RedisModuleKey *key;
-	
-	int add(double score, RedisModuleString *ele, int *flagsptr);
-	int incrby(double score, RedisModuleString *ele, int *flagsptr, double *newscore);
-	int score(RedisModuleString *ele, double *score);
-	int rem(RedisModuleString *ele, int *deleted);
-	void rangeStop();
-
-	int firstInScoreRange(double min, double max, int minex, int maxex);
-	int lastInScoreRange(double min, double max, int minex, int maxex);
-	int firstInLexRange(RedisModuleString *min, RedisModuleString *max);
-	int lastInLexRange(RedisModuleString *min, RedisModuleString *max);
-	RedisModuleString *rangeCurrentElement(double *score);
-	int rangeNext();
-	int rangePrev();
-	int rangeEndReached();
 };
 
 //---------------------------------------------------------------------------------------------
 
-struct List : Type {
-	RedisModuleKey *key;
+struct ListKey : Key {
+	ListKey(Context &ctx, String &keyname, int mode) : Key(ctx, keyname, mode) {}
+
+	int Push(int where, String &ele) {
+		return RedisModule_ListPush(_key, where, ele);
+	}
+	RedisModuleString *Pop(int where) {
+		return RedisModule_ListPop(_key, where);
+	}
+};
+
+//---------------------------------------------------------------------------------------------
+
+struct ZsetKey : Key {
+	ZsetKey(Context &ctx, String &keyname, int mode) : Key(ctx, keyname, mode) {}
 	
-	int ListPush(int where, RedisModuleString *ele);
-	RedisModuleString *ListPop(int where);
+	int Add(double score, String &ele, int *flagsptr) {
+		return RedisModule_ZsetAdd(_key, score, ele, flagsptr);
+	}
+	int Incrby(double score, String &ele, int *flagsptr, double *newscore) {
+		return RedisModule_ZsetIncrby(_key, score, ele, flagsptr, newscore);
+	}
+	int Rem(String &ele, int *deleted) {
+		return RedisModule_ZsetRem(_key, ele, deleted);
+	}
+	int Score(String &ele, double *score) {
+		return RedisModule_ZsetScore(_key, ele, score);
+	}
+
+	void RangeStop() {
+		RedisModule_ZsetRangeStop(_key);
+	}
+	int RangeEndReached(){
+		return RedisModule_ZsetRangeEndReached(_key);
+	}
+	int FirstInScoreRange(double min, double max, int minex, int maxex) {
+		return RedisModule_ZsetFirstInScoreRange(_key, min, max, minex, maxex);
+	}
+	int LastInScoreRange(double min, double max, int minex, int maxex) {
+		return RedisModule_ZsetLastInScoreRange(_key, min, max, minex, maxex);
+	}
+	int FirstInLexRange(String &min, String &max) {
+		return RedisModule_ZsetFirstInLexRange(_key, min, max);
+	}
+	int LastInLexRange(String &min, String &max) {
+		return RedisModule_ZsetLastInLexRange(_key, min, max);
+	}
+	RedisModuleString *RangeCurrentElement(double *score) {
+		return RedisModule_ZsetRangeCurrentElement(_key, score);
+	}
+	int RangeNext() {
+		return RedisModule_ZsetRangeNext(_key);
+	}
+	int RangePrev() {
+		return RedisModule_ZsetRangePrev(_key);
+	}
 };
 
 	
